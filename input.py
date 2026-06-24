ec072c31e4bb2ce335f1f52b5404f5510a46ff56
Sadra <154517183+Sadraesmizadeh89@users.noreply.github.com>
2026-06-24 11:36:09 +0330

    Update input.py

---
+++ input.py
@@ -0,0 +1,25 @@
+class Input:
+    def __init__(self,all_works):
+        self.all_works = all_works
+        self.coun = True # shorts of countinue
+
+    def get_input(self):
+        while self.coun:
+            input_work = input('Enter your work you want do it to day :')  # a b c d
+
+            self.all_works.append(input_work)
+            self.countinue()
+
+    def countinue(self):
+        desire_to_continue = input('Do you want to countinue?(yes/no)')
+
+        if desire_to_continue == 'yes':
+            print('Ok continiue!')
+            return self.get_input()
+        elif desire_to_continue == 'no':
+            print('This step is end')
+            self.coun = False
+            return True
+        else:
+            print('Enter a valid value')
+            return self.countinue()
