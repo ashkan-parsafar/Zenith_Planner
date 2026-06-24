fc453b511f5e5c9eee95b706e580cc42c462fb16
AshkanParsafar <161747971+ashkan-parsafar@users.noreply.github.com>
2026-06-24 14:48:12 +0330

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 class Priority:
-    def __init__(self,all_works):
+    def __init__(self, all_works, priority):
         self.all_works = all_works
-        self.priority = []
+        self.priority = priority
         for j in range(len(self.all_works)):
             self.priority.append(chr(97+j))
     
@@ -10,8 +10,13 @@ class Priority:
             print(i)
             work_index = int(input(f'This is something you want to do for the umpteenth time؟(you can enter the number betwen 1 to {len(self.all_works)})'))  # a  = 3
 
-            work_index = str(work_index - 1)  # 2
-            self.priority[work_index].append(i)
+            if type(work_index) != int and len(self.all_works) < work_index < 1:
+                print('your value is invalid ! pleas enter a valid value')
+                self.priority_works()
 
+            work_index = work_index - 1  # 2
+            self.priority[work_index] = (i)
+        
+        return True
         
             
